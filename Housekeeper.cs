e0a4f32ae7228fc06501ef96e82b5f7f4ef39653
Elvira Mariesdotter <elviramariesdotter@gmail.com>
2025-09-30 20:32:38 +0200

    Gave Housekeeper some new properties and methods to finish step 6

--- Housekeeper.cs
+++ Housekeeper.cs
@@ -8,9 +8,23 @@ namespace The_OOP_Hotel_part_two
 {
     internal class Housekeeper : Employee
     {
+        enum CleaningSpeed { Fast, Average, Thorough }
+        CleaningSpeed CurrentCleaningSpeed { get; set; }
+        public List<string> SpecialityAreas { get; set; } = new List<string>();
+        public Dictionary<string, int> SuppliesInventory { get; set; } = new Dictionary<string, int>();
         public override void DoWork()
         {
             Console.WriteLine($"The housekeeper {Name} is cleaning the lobby.");
+        }
+
+        public void AddSpecialityArea(string area)
+        {
+            SpecialityAreas.Add(area);
+        }
+
+        public void UpdateSuppliesInventory(string item, int quantity)
+        {
+            SuppliesInventory.Add(item, quantity);
         }
     }
 }
